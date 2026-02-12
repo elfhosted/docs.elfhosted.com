27d3549d0af5ca8383667501f706a596488ac704
Elfbot <elfbot@elfhosted.com>
2026-02-12 14:02:44 +1300

    update build.sh
    Signed-off-by: Elfbot <elfbot@elfhosted.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,22 +10,8 @@ set -x # debug failed builds
 # Run python build script to check for errors
 # python3 scripts/build.py mkdocs.yml
 
-brew install pngquant
-
-# install mkdocs (or insiders version, if we're passed a GH_TOKEN var)
-if [ -z "$GH_TOKEN" ]
-then
-  echo "No GH_TOKEN passed, doing a normal build.."
-  pip install mkdocs-material
-  mkdocs build -f mkdocs.yml
-else
-  echo "GH_TOKEN passed, doing an insiders build.."
-
-  # Try the latest
-  pip install git+https://${GH_TOKEN}@github.com/squidfunk/mkdocs-material-insiders.git
-  # pip install git+https://${GH_TOKEN}@github.com/squidfunk/mkdocs-material-insiders.git@9.5.2-insiders-4.47.1
-  mkdocs build -f mkdocs.yml
-fi
+# brew install pngquant
+pip install mkdocs-material
 
 # Setup any necessary netlify redirects
 cp netlify_redirects.txt site/_redirects
